2181113fd424a99a8caaab04ade73b4e7409e300
Bluesxz <1252455742@qq.com>
2016-03-20 23:05:14 +0800

    111

---
+++ clienttest.go
@@ -0,0 +1,59 @@
+// net project main.go
+package main
+
+
+
+import (
+	"time"
+	"github.com/go/pake"
+	"encoding/json"
+	"fmt"
+	"github.com/go/net"
+)
+
+
+func main() {
+	 
+	fmt.Println("start")
+	
+	login:=pake.LoginReq{1,2,"sxz"}
+	ctx :=pake.ContextInfo{}
+	ctx.SetSess(nil)
+	
+	ctx.SetUserId(9999)
+	mes :=&pake.Messages{ctx}
+	
+	
+	id:=pake.PakeId(1)
+	b,_:=json.Marshal(login)
+	buf:=mes.Encode(id,b)
+	fmt.Println(login,mes,buf)
+	go net.GloablTimingWheel.Run()
+	c :=net.NewClient("tcp","127.0.0.1:9000")
+	err :=c.ConnetcTimeOut(5*time.Second)
+	if err!=nil {
+		fmt.Println("c.ConnetcTimeOut err|",err)
+		return 
+	}
+	
+	err=c.Send(buf)
+	if err!=nil {
+		fmt.Println("c.Send err|",err)
+		return 
+	}
+	/*var receiveBuf []byte
+	err =c.Receive(&receiveBuf)
+	if err!=nil {
+		fmt.Println("c.Receive err|",err)
+		return 
+	}
+	fmt.Println("receiveBuf:",receiveBuf)
+	
+	var rsp pake.LoginRsp
+	p:=mes.Decode(receiveBuf)
+	json.Unmarshal(p.GetBody(),&rsp)
+	
+	fmt.Println("rsp:",rsp)
+	c.Close()*/
+	time.Sleep(time.Second*10)
+}
