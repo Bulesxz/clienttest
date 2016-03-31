d35e05990d0684210753975378951bb29e1c933f
cout_helloworld <1252455742@qq.com>
2016-03-31 21:00:19 +0800

    111

--- clienttest.go
+++ clienttest.go
@@ -1,62 +1,72 @@
 // net project main.go
 package main
 
-
-
 import (
-	"time"
-	"github.com/Bulesxz/go/pake"
+	"benchmark"
 	"encoding/json"
 	"fmt"
 	"github.com/Bulesxz/go/net"
+	"github.com/Bulesxz/go/pake"
+	"time"
 )
 
-
 func main() {
-	 
+
 	fmt.Println("start")
-	
-	login:=pake.LoginReq{1,2,"sxz"}
-	ctx :=pake.ContextInfo{}
+
+	login := pake.LoginReq{1, 2, "sxz"}
+	ctx := pake.ContextInfo{}
 	ctx.SetSess(nil)
-	
+
 	ctx.SetUserId(9999)
-	mes :=&pake.Messages{ctx}
-	
-	
-	id:=pake.PakeId(1)
-	b,_:=json.Marshal(login)
-	buf:=mes.Encode(id,b)
-	fmt.Println(login,mes,buf)
+	mes := &pake.Messages{ctx}
+
+	id := pake.PakeId(1)
+	b, _ := json.Marshal(login)
+	buf := mes.Encode(id, b)
+	fmt.Println(login, mes, buf)
 	go net.GloablTimingWheel.Run()
-	c :=net.NewClient("tcp","127.0.0.1:9000")
-	err :=c.ConnetcTimeOut(5*time.Second)
-	if err!=nil {
-		fmt.Println("c.ConnetcTimeOut err|",err)
-		return 
-	}
-	begin := time.Now().UnixNano()
-	times:=200000
-	for i:=0;i<times;i++{
-		
-		recvBuf,err:=c.SendTimeOut(2*time.Second,buf)
-		if err!=nil {
-			fmt.Println("c.Send err|",err)
-			return 
+
+	connChan := make(chan *net.Client, 1000)
+	for i := 0; i < 1000; i++ {
+		c := net.NewClient("tcp", "127.0.0.1:9000")
+		err := c.ConnetcTimeOut(5 * time.Second)
+		if err != nil {
+			fmt.Println("c.ConnetcTimeOut err|", err)
+			return
 		}
-		if recvBuf!=nil{
+		connChan <- c
+		defer c.Close()
+	}
+	f := func() bool {
+		var c *net.Client
+		select {
+			case c = <-connChan:
+			default: return false
+		}
+		recvBuf, err := c.SendTimeOut(2*time.Second, buf)
+		if err != nil {
+			fmt.Println("c.Send err|", err)
+			return false
+		}
+		if recvBuf != nil {
 			var rsp pake.LoginRsp
-			p:=mes.Decode(recvBuf)
-			json.Unmarshal(p.GetBody(),&rsp)
-			
-			fmt.Println("rsp:",rsp)
+			p := mes.Decode(recvBuf)
+			err = json.Unmarshal(p.GetBody(), &rsp)
+			if err != nil {
+				return false
+			}
+			//fmt.Println("rsp:", rsp)
+		} else {
+			return false
 		}
-		
+		connChan <- c
+		return true
 	}
-	end := time.Now().UnixNano()
-	usetime := float64(end-begin) / float64(1000000)
-	fmt.Println("qps ",float64(times)/(usetime/1000))
-	fmt.Println("tps ",usetime/float64(times),"ms")
-	c.Close()
+	n := 10000
+	usetime, failedNum := benchmark.BenchmarkFunc(n, 100, f)
+	fmt.Println("proxy usetime=", usetime, " failedNum=", failedNum, " qps=", float64(n)/(usetime/1000), " tps=", (usetime/float64(n))/1000)
+
+	
 	//time.Sleep(time.Second*10)
 }
